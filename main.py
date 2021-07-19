66eee2a915abc4f554dcec982f742e3d340c553f
Gesley Peres de Araujo <gesley531@gmail.com>
2021-07-19 16:25:02 -0300

    Times Tables v2

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
+# Times Tables v2
+from time import sleep
+num = int(input('\nEnter a number and I show the times tables: '))
 
-# First message
-T = int(input('\nIm a genius, type any number and Ill say'
-              ' his times tables up to 10: '))
-
-# calculations
-t1 = T * 1
-t2 = T * 2
-t3 = T * 3
-t4 = T * 4
-t5 = T * 5
-t6 = T * 6
-t7 = T * 7
-t8 = T * 8
-t9 = T * 9
-t10 = T * 10
-
-# Print on screen
-
-print('\n')
-
-print('-' * 12)
-
-print(f'{T}  X  1 = {t1}'
-      f'\n{T}  X  2 = {t2}'
-      f'\n{T}  X  3 = {t3}'
-      f'\n{T}  X  4 = {t4}'
-      f'\n{T}  X  5 = {t5}'
-      f'\n{T}  X  6 = {t6}'
-      f'\n{T}  X  7 = {t7}'
-      f'\n{T}  X  8 = {t8}'
-      f'\n{T}  X  9 = {t9}'
-      f'\n{T}  X 10 = {t10}')
-
-print('-' * 12)
-
+for c in range(1, 101):
+    print(f'\n{num} X {c} = {num * c}')
+    sleep(1)
